247939a0f0c7890bb9fdd14f5fa59d9262e75a52
Brian Hines <brian@projectweekend.net>
2015-07-12 13:32:16 -0500

    Validation

--- app/deck/handlers.py
+++ app/deck/handlers.py
@@ -1,12 +1,13 @@
 import falcon
 from psycopg2 import IntegrityError
 from app.utils.misc import make_code
+from app.utils.hooks import api_key_required
+from app.deck.validation import DeckValidationMixin
 
 
-class DeckCollection(object):
+@falcon.before(api_key_required)
+class DeckCollection(DeckValidationMixin):
 
     def on_post(self, req, res):
-        req.context['result'] = {
-            'code': make_code()
-        }
+        # TODO: add to database
         res.status = falcon.HTTP_CREATED
