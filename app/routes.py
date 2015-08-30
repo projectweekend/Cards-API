6f9dac5151ed51abb7ffc6ac2f83def4b7d27ec1
Brian Hines <brian@projectweekend.net>
2015-08-30 11:55:35 -0500

    Nuke it

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,7 @@
 from app import api
-from deck import handlers as deck_handlers
 
 
-api.add_route('/deck', deck_handlers.DeckCollection())
+# api.add_route('/deck', deck_handlers.DeckCollection())
 # api.add_route('/deck/{deck_id}', deck_handlers.DeckItem())
 # api.add_route('/deck/{deck_id}/shuffle', deck_handlers.DeckItemShuffle())
 # api.add_route('/deck/{deck_id}/draw', deck_handlers.DeckItemDraw())
