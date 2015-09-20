9052fe1d67045570d5f3932ef7f5dd7df2fa5d4b
Brian Hines <brian@projectweekend.net>
2015-09-19 19:06:03 -0500

    Space

--- app/deck/handlers.py
+++ app/deck/handlers.py
@@ -8,18 +8,14 @@ class DeckCollection(ValidationMixin):
     def on_post(self, req, res):
         count = req.context['data']['count']
         api_key = req.context['api_key']
-
         deck_of_cards = DeckOfCards(api_key=api_key, count=count)
         deck_of_cards.save(cursor=self.cursor)
-
         req.context['result'] = deck_of_cards.to_response_dict()
         res.status = falcon.HTTP_201
 
     def on_get(self, req, res):
         api_key = req.context['api_key']
-
         decks_of_cards = DeckOfCards.get_list_from_db(cursor=self.cursor, api_key=api_key)
-
         req.context['result'] = [deck.to_response_dict() for deck in decks_of_cards]
         res.status = falcon.HTTP_200
 
@@ -28,7 +24,6 @@ class DeckItem(object):
 
     def on_get(self, req, res, deck_id):
         api_key = req.context['api_key']
-
         try:
             deck_of_cards = DeckOfCards.get_one_from_db(
                 cursor=self.cursor,
